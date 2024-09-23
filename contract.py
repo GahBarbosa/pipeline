bbde12e2bd5aa57d54fd5a64eebfc9b73824e8a7
Gabriel Barbosa <gahbarbosa7@gmail.com>
2024-09-23 18:27:39 -0300

    23-09

---
+++ contract.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr, PositiveFloat, PositiveInt
+from datetime import datetime, time
+from enum import Enum
+
+class ProductEnum(str, Enum):
+    produto1 = "Smartphone"
+    produto2 = "Laptop"
+    produto3 = "Fones de Ouvido"
+
+class Sales(BaseModel):
+    """
+    Modelo de dados para as vendas.
+
+    Args:
+        email (EmailStr): email do comprador
+        data (datetime): data da compra
+        valor (PositiveFloat): valor da compra
+        produto (PositiveInt): nome do produto
+        quantidade (PositiveInt): quantidade de produtos
+        produto (ProdutoEnum): categoria do produto
+    """
+    email: EmailStr
+    date: datetime
+    price: PositiveFloat
+    quantity: PositiveInt
+    product: ProductEnum
---
+++ frontend.py
@@ -0,0 +1,36 @@
+import streamlit as st  
+from contract import Sales
+from datetime import datetime, time
+from pydantic import ValidationError 
+from database import store_sale
+
+def main():
+  st.title("Sistema de CRM")
+  email = st.text_input("Email do Vendedor")
+  date = st.date_input("Data da Compra", datetime.now())
+  hour = st.time_input("Hora da Compra")
+  price = st.number_input("Valor da Venda", min_value=0.0, format="%.2f")
+  quantity = st.number_input("Quantidade de Produtos", min_value=1, step=1)
+  product = st.selectbox("Produto",["Smartphone", "Laptop", "Fones de Ouvido"])
+  
+  if st.button("Salvar"):
+    try:
+
+      date_hour = datetime.combine(date,hour)
+
+      sale = Sales(
+        email=email,
+        date=date_hour,
+        price=price,
+        quantity=quantity,
+        product=product
+      )
+      
+    except ValidationError as e:
+      st.error(f"Erro na validação dos dados: {e}")
+    except Exception as e:
+      st.error(f"Erro ao salvar os dados: {e}")
+
+
+if __name__=="__main__":
+  main()
